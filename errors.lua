fc2572530ffdeef157df7df61d37a5ed2c23bf6a
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2020-01-17 19:36:24 +0200

    unimportant

--- errors.lua
+++ errors.lua
@@ -64,7 +64,9 @@ local function class_table(s)
 end
 
 local function is_error_of(e, classes)
-	return is_error(e) and (not classes or class_table(classes)[e.__index] or false)
+	if not is_error(e) then return false end
+	if not classes then return true end
+	return class_table(classes)[e.__index] or false
 end
 
 function error:__call(arg1, ...)
@@ -90,7 +92,11 @@ end
 local function pass(classes, ok, ...)
 	if ok then return true, ... end
 	local e = ...
-	if is_error_of(e, classes) then
+	if not classes then
+		if not is_error(e) then --catch-all with non-object error: catch
+			return false, e
+		end
+	elseif is_error_of(e, classes) then
 		return false, e
 	end
 	lua_error(e)
